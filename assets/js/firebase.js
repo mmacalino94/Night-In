0189848d0eadd77596ce269680036e7f3b7f4379
Karsten @ Ubuntu on Dell Latitude e6430s <karstenrabe91@gmail.com>
2018-07-28 13:50:10 -0400

    Add firebase login with email, github, google. Sign in and out

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -9,16 +9,26 @@ var config = {
     messagingSenderId: "115173215878"
 };
 firebase.initializeApp(config);
+var db = firebase.database();
+console.log(db);
+
 
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
 var uiConfig = {
     callbacks: {
         signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+            console.log(authResult);
+            $("#authentication-container").toggle();
+            removeSignInLink();
+            addLogoutLink();
+            addProfilePic(authResult);
+            // db.ref().push("Gloop");
+
             // User successfully signed in.
             // Return type determines whether we continue the redirect automatically
             // or whether we leave that to developer to handle.
-            return true;
+            return false;
         },
         uiShown: function () {
             // The widget is rendered.
@@ -28,20 +38,61 @@ var uiConfig = {
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
-    signInSuccessUrl: '<url-to-redirect-to-on-success>',
+    signInSuccessUrl: 'localhost:8000',
     signInOptions: [
         // Leave the lines as is for the providers you want to offer your users.
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-        firebase.auth.TwitterAuthProvider.PROVIDER_ID,
         firebase.auth.GithubAuthProvider.PROVIDER_ID,
         firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        firebase.auth.PhoneAuthProvider.PROVIDER_ID
+        // firebase.auth.PhoneAuthProvider.PROVIDER_ID
     ],
     // Terms of service url.
-    tosUrl: '<your-tos-url>',
+    tosUrl: 'https://krab7191.github.io/Night-In/',
     // Privacy policy url.
-    privacyPolicyUrl: '<your-privacy-policy-url>'
+    privacyPolicyUrl: 'https://krab7191.github.io/Night-In/'
 };
 
 ui.start('#firebaseui-auth-container', uiConfig);
+
+// Make only signed in users save stuff
+firebase.auth().onAuthStateChanged(function (user) {
+    window.user = user; // user is undefined if no user signed in
+    if (window.user) {
+        console.log("I'm logged in!!");
+        console.log(window.user);
+    }
+    else {
+        console.log("log in please...");
+    }
+});
+
+function removeSignInLink() {
+    $("#login").remove();
+}
+function addLogoutLink() {
+    var $logout = $("<p>").attr("id", "logout").html("Log Out");
+    $("#authbar").append($logout);
+}
+function addProfilePic(userData) {
+    var $img = $("<img>").attr("id", "profile-img");
+    var src = userData.additionalUserInfo.profile.picture;
+    $img.attr("src", src);
+    $("#authbar").append($img);
+    $("#title").css("margin-top", "50px");
+}
+
+$("#login").on("click", function () {
+    $("#authentication-container").toggle();
+});
+$(document).on("click", "#logout", function () {
+    firebase.auth().signOut().then(function() {
+        console.log("signed out successfully");
+        $("#profile-img").remove();
+        $("#logout").remove();
+        var $login = $("<p>").attr('id', "login").html("Log In / Sign Up");
+        $("#authbar").append($login);
+    }).catch(function (err) {
+            // Handle errors
+            console.log("Error signing out");
+        });
+});
