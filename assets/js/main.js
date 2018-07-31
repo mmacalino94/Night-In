8ee251501c034c7bcddf50ed08ac21c0961ba755
Karsten @ Ubuntu on Dell Latitude e6430s <karstenrabe91@gmail.com>
2018-07-31 00:58:10 -0400

    Clean up firebase code, remove commented lines. Put tweaks.js into main js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -115,6 +115,16 @@ $(function () {
 
 // End MODAL functions
 
+// Prevent default on form submition
+$("#food-input").keydown(function (event) {
+    if (event.keyCode == 13) {
+        event.preventDefault();
+        var food = $("#food-input").val();
+        // Handle food mapping function here...
+        console.log(food);
+        return false;
+    }
+});
 
 //Randomize Button Function
 var $beerInput = $('#beer-checkbox');
